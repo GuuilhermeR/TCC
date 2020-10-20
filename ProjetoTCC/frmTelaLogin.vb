42cb0d120fc03e1d1a34c92fed7067afd98fdac0
Guilherme Rudiger <guilherme.rudiger@movtech.inf.br>
2020-10-20 12:59:51 -0300

    Configurações de conexão e testes de conexões

--- ProjetoTCC/frmTelaLogin.vb
+++ ProjetoTCC/frmTelaLogin.vb
@@ -1,9 +1,12 @@
-﻿Public Class frmTelaLogin
+﻿Imports System.Data.SQLite
+
+Public Class frmTelaLogin
     Private Sub frmTelaLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         fINIConexao()
 
         Me.txtSenha.TextBoxValue.PasswordChar = "*"
+
 
     End Sub
 
@@ -13,14 +16,31 @@
 
     Private Sub btnLogar_Click(sender As Object, e As EventArgs) Handles btnLogar.Click
 
-        If Me.txtUsuario.Text = "" AndAlso Me.txtSenha.Text = "" Then
+        Dim strSQL As String = $"SELECT * FROM Login WHERE usuario = {Me.txtUsuario.Text} AND senha = {Me.txtSenha.Text}"
+
+        Dim dr As IDataReader = conexaoPadrao.ReturnDataReader(strSQL)
+
+        If dr.Read Then
             Me.Hide()
             frmMenuPrincipal.Show()
-
         ElseIf IsNullOrEmpty(Me.txtSenha.Text) AndAlso IsNullOrEmpty(Me.txtUsuario.Text) Then
             MsgBox("Favor preencher os campos")
         Else
             MsgBox("Senha Errada")
         End If
+
     End Sub
 End Class
+
+
+'Dim myConnString1 As String = "DataSource=C:\BancoDadosTCC.db;"
+'Dim sqConnection1 As New SQLiteConnection(myConnString1)
+'Dim myInsertQuery As String = "SELECT * FROM Login"
+'Dim sqCommand As New SQLiteCommand(myInsertQuery)
+'sqCommand.Connection = sqConnection1
+'sqConnection1.Open()
+'Try
+'    sqCommand.ExecuteNonQuery()
+'Finally
+'    sqConnection1.Close()
+'End Try
